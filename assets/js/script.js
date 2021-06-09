cfb637532551b21861a29a1ddc7bd63a18bdb7ab
Felipe-M-dev <fmartinez.redes@gmail.com>
2021-06-09 12:18:16 -0400

    Update script.js index.html
    - add go up float button

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,4 +121,28 @@ $(document).ready(function () {
         });
     });
 
+    /**Scroll up button**/
+
+    document.getElementById("button-up").addEventListener("click", scrollUp);
+
+    function scrollUp(){
+        var currentScroll = document.documentElement.scrollTop;
+        if (currentScroll > 0) {
+            window.requestAnimationFrame(scrollUp);
+            window.scrollTo (0, currentScroll - (currentScroll / 20));
+            buttonUP.style.transform = "scale(0)";
+        }
+    }
+
+    buttonUP = document.getElementById("button-up");
+
+    window.onscroll = function() {
+        var scroll = document.documentElement.scrollTop;
+        if (scroll > 500) {
+            buttonUP.style.transform = "scale(1)";
+        }else if (scroll = 500) {
+            buttonUP.style.transform = "scale(0)";
+        }
+    }
+  
 });
